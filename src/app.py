06765d49801e04a3c8605737f794b5635ca6f628
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2023-07-11 14:25:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+DiscordClientSecret = "8dyfuiRyqFvVc3RRr_edRk-fZ__JItpP" ;
+aws_access_key = "AKIASP2TPHJS3AVMLWWW",
+aws_access_token = "AKIASP2TPHJS3AVMLYYY"
 
 @app.route("/")
 def index():
